1edbeedd18ed427d89c6eec06d372ae790fdb7b4
kjk1208 <kjk1208@dgist.ac.kr>
2022-04-29 01:32:46 +0900

    branch test

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const loginForm = 
+
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick(){
+    console.log(loginInput.value);
+    console.log("click!!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
