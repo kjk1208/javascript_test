1b82500ab55d0335c7e09f52da91b3c122b9545f
kjk1208 <kjk1208@dgist.ac.kr>
2022-05-11 17:45:28 +0900

    20220511_7_2_until

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,28 @@ const toDoInput = toDoform.querySelector("input");
 //const toDoInput = document.querySelector("#todo-form input");
 const toDolist = document.getElementById("todo-list");
 
-function handleToDoSubmit(event){
-    event.preventDefault();
-    toDoInput.value = "";
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
 }
 
-toDoform.addEventListener("submit", handleTodoSubmit);
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌"
+    button.addEventListener("click",deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);    
+    toDolist.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;    
+    toDoInput.value = "";
+    paintToDo(newTodo);
+}
+
+toDoform.addEventListener("submit", handleToDoSubmit);
